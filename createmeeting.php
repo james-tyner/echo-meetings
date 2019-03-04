32ca6d66d1e941e71dec2a400e2bfdde0cd70865
Jessica Jones <kazaroff@usc.edu>
2019-03-04 15:04:58 -0800

    createmeeting

---
+++ createmeeting.php
@@ -0,0 +1,206 @@
+<<?php
+$title = "Home"; // title of the page is the heading1 and also appears in meta title
+$username = "James"; // this will appear in the bottom of the sidebar. Eventually should be managed by session variables
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang='en'>
+ <head>
+ 	<?php require 'components/metadata.php'; ?>
+ </head>
+ <style>
+ h2{
+   position: absolute;
+width: 275px;
+height: 23px;
+left: 25%;
+top: 90%;
+
+font-family: Rubik;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+font-size: 24px;
+
+color: #222222;
+ }
+
+
+
+ input{
+   position: absolute;
+width: 300px;
+height: 40px;
+left: 25%;
+top: 150%;
+border-radius: 8px;
+text-indent: 8px;
+font-size: 15px;
+box-shadow:  10px  rgba(0,0,0,0.1);
+
+
+ }
+
+ input::placeholder{
+   font-style: italic;
+ }
+
+ #at{
+   position: absolute;
+width: 100px;
+height: 40px;
+left: 120%;
+top: 160%;
+
+color: #777777;
+
+
+ }
+
+
+
+ #time h2{
+   position:absolute;
+   left: 130%;
+   top: 90%;
+
+ }
+
+ #time input{
+   left: 130%;
+   top: 150%;
+ }
+
+ #location h2{
+   position:absolute;
+  left: 25%;
+   top: 220%;
+
+ }
+
+ #location input{
+   left: 25%;
+   top: 280%;
+   width: 500px;
+ }
+
+button{
+  position: absolute;
+width: 200px;
+height: 44px;
+left: 90%;
+top: 360%;
+background: #00D9BD;
+border-radius: 50px;
+color: white;
+font-size: 14px;
+font-family: Rubik;
+}
+
+#side-line{
+  position: absolute;
+width: 180%;
+height: 0px;
+left: 150%;
+top: 230%;
+
+/* Gray/3 */
+border: 0.5px solid #777777;
+transform: rotate(90deg);
+}
+
+#time input::before {
+
+    color: #777777;
+
+  }
+
+  #add-team h3{
+    position: absolute;
+width: 277px;
+height: 60px;
+left: 260%;
+top: 70%;
+font-family: Rubik;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+font-size: 18px;
+
+color: #000000;
+  }
+
+  #add-team input{
+    left: 248%;
+    top: 120%;
+    width: 200px;
+    height: 30px;
+    font-size: 11px;
+    border: none;
+    border-bottom: 1px solid #000000;
+    box-shadow: none;
+    border-radius: 0px;
+    background: transparent;
+    opacity: 0%;
+
+  }
+
+i{
+  margin-left: 305%;
+
+}
+
+
+ </style>
+ <body>
+ 	<!-- navbar -->
+ 	<?php require 'components/nav.php'; ?>
+
+ 	<!-- middle area -->
+ 	<main>
+ 		<header>
+ 			<h1 class="page-title">Create Meeting</h1>
+ 		</header>
+
+ 		<section>
+      <div id="name">
+      <h2>Meeting Name</h2>
+      <input type="text" name="name" placeholder="set meeting name" ></input>
+    </div>
+
+<div id="at">at</div>
+
+    <div id="time">
+      <h2>Time</h2>
+      <input type="datetime-local" name="name"  ></input>
+    </div>
+
+    <div id="location">
+      <h2>Location</h2>
+      <input type="text" name="name" placeholder="set location" ></input>
+    </div>
+
+    <div id="button">
+      <button type="button">Create New Meeting</button>
+    </div>
+
+<hr id="side-line">
+
+ 		</section>
+
+    <section>
+
+      <div id="add-team">
+
+        <h3>Add Team</h2>
+
+        <input type="text" name="name" placeholder="search teams" ><i class="fa fa-search" aria-hidden="true" ></i></input>
+
+      </div>
+
+    </section>
+ 	</main>
+
+ </body>
+ </html>
