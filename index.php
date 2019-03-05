a77f07dcfd39375fa6650bbc547c76f0f2ed001a
Mars Tan <jianxuat@usc.edu>
2019-03-04 19:56:19 -0800

    setup

--- index.php
+++ index.php
@@ -9,22 +9,44 @@ $active = "dashboard"; // controls which sidebar item is highlighted. Options ar
 <!DOCTYPE html>
 <html lang='en'>
 <head>
-	<?php require 'components/metadata.php'; ?>
+  <?php require 'components/metadata.php'; ?>
 </head>
 <body>
-	<!-- navbar -->
-	<?php require 'components/nav.php'; ?>
+<!-- navbar -->
+<?php require 'components/nav.php'; ?>
 
-	<!-- middle area -->
-	<main>
-		<header>
-			<h1 class="page-title">Dashboard</h1>
-		</header>
+<!-- middle area -->
+<main>
+  <header>
+    <h1 class="page-title">Dashboard</h1>
+  </header>
 
-		<section>
+  <section>
+    <div class="up-next-div">
+      <h2>Up Next</h2>
+      <div class="card color-band">
+        <h3 class="card-team">ITP 460</h3>
+        <h2 class="card-heading">Update completed tasks to use React</h2>
+        <div class="card-icons">
+          <div class="icon-group"><i class="fas fa-user-circle"></i> 2</div>
+          <div class="icon-group"><i class="fas fa-comment"></i> 8</div>
+          <div class="icon-group"><i class="fas fa-clock"></i> tomorrow</div>
+        </div>
+      </div>
 
-		</section>
-	</main>
+    </div>
+
+    <div class="task-team-flexbox">
+      <div class="tasks-div">
+        <h2>Tasks</h2>
+      </div>
+      <div class="team-div">
+        <h2>Team</h2>
+      </div>
+    </div>
+
+  </section>
+</main>
 
 </body>
 </html>
