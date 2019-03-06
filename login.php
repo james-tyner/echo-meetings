51a45be13abd5b583b2b918fb3f34374e7c21118
James Tyner <services@jamestyner.com>
2019-03-05 22:59:09 -0800

    Corrected login/signup pages

--- login.php
+++ login.php
@@ -8,40 +8,37 @@
 <html lang='en'>
 <head>
 	<?php require 'components/metadata.php'; ?>
-
 </head>
 <body id="login-signup-main">
-	<div class="input-window">
-		<div id="logo"> <img src="assets/Logo on White.svg" alt="logo-img"></div>
-		<br>
-		<div id="box">
-			<h1>Welcome Back!</h1>
-			<h2>Sign in to echo</h2>
-			<a href="#"><div id="google"><img src="assets/sign-google.png" alt="Sign-in with Google"></div></a>
-			<p>Or use your email account:</p>
-			<div class="entry-row">
-				<img src="assets/contact.png" alt="email icon">
-				<input type="text" name="email" value="" placeholder="Email">
-			</div>
-			<br>
-			<div class="entry-row">
-				<img src="assets/lock.png" alt="lock icon">
-				<input type="text" name="password" value="" placeholder="Password">
-			</div>
-			<br>
-			<div id="button">
-				<a href="#"><button type="button">SIGN IN</button></a>
-			</div>
+	<div id="logo"> <img src="assets/Logo on White.svg" alt="logo-img"></div>
+	<div class="bg-window" id="login-bg">
+		<div id="outside-prompt">
+			<p>Already have an account?</p>
+			<a href="signup.php" class="prompt-button" id="signup-prompt" type="button">SIGN UP</a>
 		</div>
 	</div>
-	<div class="small-window">
-		<br><br><br>
-		<div id="box2">
-			<p>Create an account to start organizing your meetings.</p>
-			<div id="button">
-				<a href="SignUp-1.5.html"><button type="button">SIGN UP</button></a>
+	<div class="input-window" id="login-input">
+		<div id="input-container">
+			<h1>Create Your Account</h1>
+			<a href="#"><img id="google" src="assets/google-sign-in@2x.png" alt="Sign-in with Google"></a>
+
+			<p>Or register with your email account:</p>
+			<div class="entry-row">
+				<i class="fas fa-signature"></i>
+				<input type="text" name="name" value="" placeholder="Name">
+			</div>
+			<div class="entry-row">
+				<i class="fas fa-at"></i>
+				<input type="text" name="email" value="" placeholder="Email">
+			</div>
+			<div class="entry-row">
+				<i class="fas fa-key"></i>
+				<input type="text" name="password" value="" placeholder="Password">
+			</div>
+			<a href="index.php" class="input-button" id="signin-button">SIGN IN</a>
 			</div>
 		</div>
+
 	</div>
 </body>
 </html>
