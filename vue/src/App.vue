75cfe1567e7b683dc49ce9a0c0753b26e8e71374
Jianxuan Tan <jianxuat@usc.edu>
2019-03-19 20:30:46 -0700

    metadata

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Navbar :items="items"></Navbar>
+    <Navbar></Navbar>
+
+    <header>
+      <h1 class="page-title">Dashboard</h1>
+    </header>
+
+    <!-- middle area -->
+    <main>
+      <section>
+        <Dashboard></Dashboard>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
+import Dashboard from './views/dashboard'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    Navbar
+    Navbar,
+    Dashboard
   },
   data: function () {
     return {
@@ -31,12 +40,4 @@ export default {
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
--- vue/src/components/Navbar.vue
+++ vue/src/components/Navbar.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="nav">
-    <h1>Menu Items</h1>
-    <p v-for="item in items" :key="item.id">{{ item.msg }}</p>
-  </div>
+  <nav id="sidebar">
+    <img id="logo" src="../assets/logo_dark.svg" alt="logo-img">
+    <div class="nav-items">
+      <a href="#" class="nav-item">Dashboard</a>
+      <a href="#" class="nav-item">Meetings</a>
+      <a href="#" class="nav-item">Tasks</a>
+      <a href="#" class="nav-item">Teams</a>
+    </div>
+    <!-- test -->
+    <!-- bottom part -->
+    <div class="profile">
+      <!-- User Photo -->
+      <div id="profile-photo"></div>
+      <div id="profile-info">
+        <!-- User Name -->
+        <a href="#"></a>
+        <!-- Sign Out -->
+        <a id="signout" href="#">Sign Out</a>
+      </div>
+    </div>
+  </nav>
+
 </template>
 
 <script>
