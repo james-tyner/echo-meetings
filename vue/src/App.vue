c267c827ea332a51b50263a5e8184fe3c86d2aa1
Jianxuan Tan <jianxuat@usc.edu>
2019-03-19 20:09:34 -0700

    init folder structure

---
+++ vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar :items="items"></Navbar>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Navbar
+  },
+  data: function () {
+    return {
+      items: [{
+        id: 1,
+        msg: 'Name 1'
+      }, {
+        id: 2,
+        msg: 'Name 2'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ vue/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav">
+    <h1>Menu Items</h1>
+    <p v-for="item in items" :key="item.id">{{ item.msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    items: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
