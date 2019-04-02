b5e0b82787fb06c6a7fa1d6aef461fe814b176d5
James Tyner <services@jamestyner.com>
2019-04-02 09:37:36 -0700

    DO NOT MERGE: Set to PHP only and added Auryc tag

--- node/src/routes/api/meeting.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const Meeting = mongoose.model('Meeting');
-
-
-module.exports = router;
--- node/src/routes/api/task.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const Task = mongoose.model('Task');
-
-
-module.exports = router;
