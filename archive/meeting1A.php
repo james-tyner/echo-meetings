470cbd7add0a8888a43d519dacb48ad5161a9ca4
Angella Qian <aqian@usc.edu>
2019-04-09 17:22:30 -0700

    Updated meetings pages with Vue

--- archive/meeting1A.php
+++ archive/meeting1A.php
@@ -24,6 +24,7 @@ $active = "meetings";
 		<div class="add-button align-right">
 		  <a href="createmeeting.php"><i class="material-icons"> add </i></a>
 		</div>
+
 		<h1 class="page-title"> Meetings </h1>
 		<div class="filter-bar">
 			<p>Filter by group:</p>
@@ -31,7 +32,6 @@ $active = "meetings";
 			<a class="filter-group" href="meeting1A_2.php">ITP 460</a>
 			<a class="filter-group" href="meeting1A_3.php">WRIT 340</a>
 			<a class="filter-group" href="meeting1A_4.php">SOCCER CLUB</a>
-		</div>
 	</header>
 
 	<!-- middle area -->
@@ -77,7 +77,9 @@ $active = "meetings";
   			<p class="mtg-date"> Mar 20</p>
   		</div>
 		</section>
-	</main>
+	</main> 
+
+
 
 </body>
 </html>
--- vue/src/views/meetings.vue
+++ vue/src/views/meetings.vue
@@ -1,46 +1,126 @@
 <template>
   <main>
+    <section>
+      <div id="filterbar">
+        Filter by group:
+        <div id="options">
+          <a class="filter-group" v-on:click="selectedTeam = ''" v-bind:class="{'selected' : selectedTeam == ''}">ALL</a>
+          <a v-for="meeting in fakeMeetings" class="filter-group" v-on:click="selectedMeeting = meeting.team.name" v-bind:class="{'selected' : selectedMeeting == meeting.team.name}">{{meeting.team.name}}</a>
+        </div> <!-- #options -->
+      </div> <!-- #filterbar -->
+    </section>
 
     <section class="meeting-cards">
-      <div class="mtg-card">
-        <div class="card green-color-band">
-          <h3 class="card-team green">ITP 460</h3>
-          <h2 class="card-heading">Work Session</h2>
-        </div>
-
-        <p class="mtg-date"> Feb 27</p>
-      </div>
-
-      <div class="mtg-card">
-        <div class="card blue-color-band">
-          <h3 class="card-team blue">WRIT 340</h3>
-          <h2 class="card-heading">Presentation Planning</h2>
-        </div>
-
-        <p class="mtg-date"> Mar 3</p>
-      </div>
-
-      <a href="meeting2.php">
-        <div class="mtg-card">
-          <div class="card blue-color-band centered">
-            <h3 class="card-team blue">WRIT 340</h3>
-            <h2 class="card-heading">Giving Project</h2>
-          </div>
-
-          <p class="mtg-date"> Mar 10</p>
-        </div>
-      </a>
-
-
-      <div class="mtg-card">
-        <div class="card red-color-band">
-          <h3 class="card-team red">SOCCER CLUB</h3>
-          <h2 class="card-heading">Playoff Logistics</h2>
-        </div>
-
-        <p class="mtg-date"> Mar 20</p>
+      <div v-for="meeting in fakeMeetings">
+        <MeetingCard v-bind:meeting="meeting"></MeetingCard>
       </div>
     </section>
     
   </main>
 </template>
+
+<script>
+  window.moment = require('moment'); // for use on MeetingCard component
+
+  import MeetingCard from "../components/meetings/MeetingCard.vue"
+  export default {
+    name: 'meetings',
+    components: {
+      MeetingCard
+    }
+  },
+  computed: {  
+  },
+  methods: {
+    filterMeetings:function(teamName) {
+      this.selectedTeam = teamName;
+    }
+  },
+  data: function() {
+    return{
+      selectedTeam:"",
+      fakeMeetings: [
+        {
+          "meetings": [{
+            "id": "1",
+            "title": "Presentation Planning",
+            "time": "1553479225106",
+            "location": "TTH 110",
+            "invitees": [{
+              "name": "Tommy Trojan",
+              "username": "trojan.echo",
+              "email": "trojan@usc.edu",
+              "image": null
+            }],
+            "agendas": [{
+              "id": "1",
+              "title": "Agenda",
+              "description": "Details",
+              "notes": "Notes",
+              "tasks": [{
+                "id": "1",
+                "name": "Task",
+                "description": "Details",
+                "due": "1553479225106",
+                "status": "1"
+              }]
+            }],
+            "team": {
+              "id": "1",
+              "name": "WRIT 340",
+              "description": "Details",
+              "color": "blue",
+              "members": [{
+                "name": "Mars Tan",
+                "username": "mars.tanjx",
+                "email": "jianxuat@usc.edu",
+                "image": null
+              }]
+            },
+            "start": "1553479225106",
+            "end": "1553480225106"
+          }]
+        },{
+          "meetings": [{
+            "id": "2",
+            "title": "Playoff Logistics",
+            "time": "1553479225106",
+            "location": "TTH 110",
+            "invitees": [{
+              "name": "Tommy Trojan",
+              "username": "trojan.echo",
+              "email": "trojan@usc.edu",
+              "image": null
+            }],
+            "agendas": [{
+              "id": "1",
+              "title": "Agenda",
+              "description": "Details",
+              "notes": "Notes",
+              "tasks": [{
+                "id": "1",
+                "name": "Task",
+                "description": "Details",
+                "due": "1553479225106",
+                "status": "1"
+              }]
+            }],
+            "team": {
+              "id": "2",
+              "name": "SOCCER CLUB",
+              "description": "Details",
+              "color": "green",
+              "members": [{
+                "name": "Mars Tan",
+                "username": "mars.tanjx",
+                "email": "jianxuat@usc.edu",
+                "image": null
+              }]
+            },
+            "start": "1553479225106",
+            "end": "1553480225106"
+          }]
+        }
+      ]
+
+</script>
