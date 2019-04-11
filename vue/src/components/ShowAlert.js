6f647a59c805d95f3ac6b18770ab4a405e57fb30
Jianxuan Tan <jianxuat@usc.edu>
2019-04-10 22:02:01 -0700

    vue slide in error modal

--- vue/src/components/ShowAlert.js
+++ vue/src/components/ShowAlert.js
@@ -1,41 +1,16 @@
-const showAlert = function(color, message){
-      // Borrowed from: https://gist.github.com/alirezas/c4f9f43e9fe1abba9a4824dd6fc60a55
+const showAlert = function (color, message, time = 5000) {
+  let modal = document.getElementById("alert-modal");
 
-      function fadeOut(el){
-        el.style.opacity = 1;
+  modal.getElementsByTagName("p")[0].innerHTML = message;
 
-        (function fade() {
-          if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-          } else {
-            requestAnimationFrame(fade);
-          }
-        })();
-      };
+  modal.classList.add(color + "-color-band");
+  modal.classList.remove('out');
+  modal.classList.add('in');
 
-      function fadeIn(el, display){
-        el.style.opacity = 0;
-        el.style.display = display || "block";
-
-        (function fade() {
-          var val = parseFloat(el.style.opacity);
-          if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-          }
-        })();
-      };
-
-      let modal = document.getElementById("alert-modal");
-
-      modal.getElementsByTagName("p")[0].innerHTML = message;
-
-      modal.classList.add(color + "-color-band");
-
-      fadeIn(modal, "block");
-      setTimeout(() => {
-        fadeOut(modal);
-      }, 2000)
-    }
+  setTimeout(() => {
+    modal.classList.remove('in');
+    modal.classList.add('out');
+  }, time)
+}
 
 export default showAlert;
