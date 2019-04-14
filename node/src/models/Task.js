8b47cbc929d921ab4141adc1bb404c9ecb099309
Jianxuan Tan <jianxuat@usc.edu>
2019-04-14 12:53:44 -0700

    node task get, post

--- node/src/models/Task.js
+++ node/src/models/Task.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
 
+const Meeting = mongoose.model('Meeting');
+
 const TaskSchema = new mongoose.Schema({
   name: String,
   description: String,
   due: Date,
   assignees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   status: { type: Number, min: 0, max: 2 },
-  team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
-  meeting: { type: mongoose.Schema.Types.ObjectId, ref: 'Meeting' },
-  // agenda: String, // TODO
+  // team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
+  meeting: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meeting',
+    validate: {
+      validator: _id => Meeting.countDocuments({ _id }).then(doc => doc > 0),
+      message: 'Meeting non existent',
+    },
+  },
+  agenda: { type: mongoose.Schema.Types.ObjectId, ref: 'Agenda' },
 }, { timestamps: true });
+
+TaskSchema.methods.validateAgenda = function () {
+  return Meeting
+    .countDocuments({ _id: this.meeting, 'agendas._id': { $in: [this.agenda] } })
+    .then(doc => doc > 0);
+};
 
 mongoose.model('Task', TaskSchema);
--- node/src/routes/api/task.js
+++ node/src/routes/api/task.js
@@ -1,7 +1,110 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 
+const User = mongoose.model('User');
 const Task = mongoose.model('Task');
+
+const log = require('../../util/log');
+const auth = require('../auth');
+
+// always perform auth check
+router.use(auth.required, async (req, res, next) => {
+  User.findById(req.payload.id).then((user) => {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    req.locals = {};
+    req.locals.user = user;
+    next();
+  });
+});
+
+// Get all Tasks
+router.get('/', async (req, res) => {
+  const { user } = req.locals;
+  Task.find({ assignees: user.id })
+    .populate('meeting')
+    .then((tasks) => {
+      res.json({ tasks });
+    });
+});
+
+
+// Create a Task
+router.post('/', async (req, res) => {
+  const { user } = req.locals;
+  log.log('Creating a task');
+  const req_task = req.body.task;
+  if (!req_task) {
+    return res.status(422).json({
+      errors: {
+        message: 'no task field',
+      },
+    });
+  }
+  // required fields: name, meeting, agenda
+  if (!req_task.name) {
+    return res.status(422).json({
+      errors: {
+        message: "name can't be blank",
+      },
+    });
+  }
+  if (!req_task.meeting) {
+    return res.status(422).json({
+      errors: {
+        message: "meeting can't be blank",
+      },
+    });
+  }
+  if (!req_task.agenda) {
+    return res.status(422).json({
+      errors: {
+        message: "agenda can't be blank",
+      },
+    });
+  }
+  // build the task
+  const task = new Task();
+  task.title = req_task.title;
+  task.meeting = req_task.meeting;
+  task.agenda = req_task.agenda;
+  // only update fields that were actually passed...
+  if (typeof req_task.description !== 'undefined') {
+    task.description = req_task.description;
+  }
+  if (typeof req_task.due !== 'undefined') {
+    task.due = req_task.due;
+  }
+  task.assignees = [];
+  if (typeof req_task.assignees !== 'undefined') {
+    task.assignees = req_task.assignees;
+  }
+  task.validateAgenda().then((pass) => {
+    if (pass) {
+      // this will perform validation of meeting
+      task.save().then(() => {
+        log.log(`Task (${task.title}) created`);
+        return res.json({ task });
+      }).catch((err) => {
+        console.log(err);
+        // condense error messages
+        let message = '';
+        for (const single_error in err.errors) {
+          // TODO hasOwnProperty
+          if (err.errors.hasOwnProperty(single_error)) message += `${err.errors[single_error].message} `;
+        }
+        return res.status(422).json({
+          errors: { message },
+        });
+      });
+    } else {
+      return res.status(422).json({
+        errors: { message: 'Agenda does not exist' },
+      });
+    }
+  });
+});
 
 
 module.exports = router;
