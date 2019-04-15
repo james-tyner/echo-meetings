707ff9f99a1097aaf7700aac065d3b19c3586321
Katlyn Lee <katlynlee@Katlyns-MacBook-Air.local>
2019-04-15 09:57:14 -0700

    Updates to Login.vue
    Added some components, edited some of the text

--- vue/src/views/login.vue
+++ vue/src/views/login.vue
@@ -4,39 +4,40 @@
     <div class="login-flex">
       <div class="bg-window">
         <div id="outside-prompt">
-          <h1 class="heading1">Start Planning</h1>
-          <p>Login in with your Google Account to start organizing your meetings</p>
+          <h1 class="heading1">Streamline Your Meetings</h1>
+          <p>Whether you're planning the next big event for your club or preparing for a simple team meeting, improve your team workflows with Echo. Simply sign in with your Google account to start.</p>
          
           <a :href="login_url"><img id="google" src="/assets/google-sign-in@2x.png" alt="Sign-in with Google"></a>
         </div>
       </div>
       <div class="input-window">
          <div id="input-container">
+
             <div class="row">
               <div class="text">
-                <p class="heading3"> Plan Meetings</p>
-                <p>Plan ahead your meetings and see them all in one place.</p>
+                <p class="heading3"> Create Teams </p>
+                <p>Start by inviting members via email to your team.</p>
               </div>
-              <div><img src="/assets/meetings.png" class="card-shadow" id="image-1">
+              <div><img src="/assets/teams.png" class="card-shadow image-1">
               </div>
             </div> <!-- end of row-1 -->
 
             <div class="row">
               <div>
-                <img src="/assets/meetings.png" class="card-shadow" id="image-1">
+                <img src="/assets/meetings.png" class="card-shadow image-1">
               </div>
               <div class="text">
                 <p class="heading3"> Plan Meetings</p>
-                <p>Plan ahead your meetings and see them all in one place.</p>
+                <p>With your team, set a meeting time and organize your meeting notes with agenda items and tasks.</p>
               </div>
             </div> <!-- end of row-2 -->
 
             <div class="row">
               <div class="text">
-                <p class="heading3"> Plan Meetings</p>
-                <p>Plan ahead your meetings and see them all in one place.</p>
+                <p class="heading3"> Share Tasks</p>
+                <p>Assign different tasks from the meeting to your group members.</p>
               </div>
-              <div><img src="/assets/meetings.png" class="card-shadow" id="image-1">
+              <div><img src="/assets/meetings.png" class="card-shadow image-1">
               </div>
             </div> <!-- end of row-3 -->
           
